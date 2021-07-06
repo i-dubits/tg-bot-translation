fdcd5f93308f1313ff3f169a8ed0fefd59d72106
Ilya_D <76211620+i-dubits@users.noreply.github.com>
2021-07-06 19:58:06 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,4 +3,5 @@ TOKEN = '' #insert your Telegram bot token here
 SRC_vocab_path = './transformer/model_weights/SRC.p'	# source (russian) vocabulary look-up table (torchtext.vocab object)
 TRG_vocab_path = './transformer/model_weights/TRG.p'	# target (english) vocabulary look-up table (torchtext.vocab object)
 
-checkpoint_path = './transformer/model_weights/best-val-model_35_epoch.pt'	# model weights
+checkpoint_path = './transformer/model_weights/best-val-model_35_epoch.pt'	# model weights which should be downloaded from here 
+																			                                      # https://drive.google.com/file/d/1gj-GJJuLMO87oLQw6xqWgRU9YI3Bed_u/view?usp=sharing
